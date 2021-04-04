76f61089e04b480f0afcbaf7b54b7dce951f5905
kazirafi71 <kazirafi66@gmail.com>
2021-04-04 19:59:13 +0600

    hello

--- app.js
+++ app.js
@@ -47,13 +47,13 @@ app.use('/cart', cartRoute)
 
 //TODO: For deployment
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'))
-//     const path = require('path')
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 mongoose.set('useCreateIndex', true)
 mongoose.connect(MONGO_URI, {
